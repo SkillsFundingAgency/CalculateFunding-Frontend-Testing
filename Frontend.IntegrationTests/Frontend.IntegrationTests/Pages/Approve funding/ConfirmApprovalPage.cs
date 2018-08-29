6277011929814ffe4666451bf1979fef8e80b810
Richard Wilson <Richard@DESKTOP-34I2ESG>
2018-08-29 11:30:29 +0100

    PBI 56069 & 57342

---
+++ Frontend.IntegrationTests/Frontend.IntegrationTests/Pages/Approve funding/ConfirmApprovalPage.cs
@@ -0,0 +1,17 @@
+﻿using AutoFramework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Frontend.IntegrationTests.Pages.Approve_funding
+{
+    public class ConfirmApprovalPage
+    {
+        public ConfirmApprovalPage()
+        {
+            PageFactory.InitElements(Driver._driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "approve")]
+        public IWebElement confirmApprovalButton { get; set; }
+    }
+}
