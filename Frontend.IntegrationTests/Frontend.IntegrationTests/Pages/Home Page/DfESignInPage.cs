ff4f2df77c728904f5bd4c232608232a48b0754a
Richard Wilson <Richard@DESKTOP-34I2ESG>
2018-09-05 16:33:24 +0100

    AD Sign in changes

---
+++ Frontend.IntegrationTests/Frontend.IntegrationTests/Pages/Home Page/DfESignInPage.cs
@@ -0,0 +1,23 @@
+﻿using AutoFramework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Frontend.IntegrationTests.Pages.Home_Page
+{
+    public class DfESignInPage
+    {
+        public DfESignInPage()
+        {
+            PageFactory.InitElements(Driver._driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "userNameInput")]
+        public IWebElement userNameInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "passwordInput")]
+        public IWebElement passwordInput { get; set; }
+
+        [FindsBy(How = How.Id, Using = "submitButton")]
+        public IWebElement submitButton { get; set; }
+    }
+}
