053ac035384c25366808fe1948bfe5f2f00e44d3
Richard Wilson <richard.wilson@education.gov.uk>
2018-11-06 15:37:57 +0000

    PBI 62977 & 62987 - Role Based Permissions

---
+++ Frontend.IntegrationTests/Frontend.IntegrationTests/Tests/Features/UserPermissions.feature
@@ -0,0 +1,14 @@
+﻿Feature: UserPermissions
+	
+@Workitem 62977 62987 Driver
+Scenario Outline: Update Permissions
+Given the user '<userId>' has the following permissions for Funding Stream '<fundingStreamId>'
+| Permission             | Granted |
+| CanEditSpecification   | true    |
+| CanCreateSpecification | true	   | 
+| CanEditCalculation	 | true	   | 
+
+
+Examples:
+| userId                               | fundingStreamId |
+| 40a8a720-2817-430e-9be1-98d337fd01f4 | PSG             |
---
+++ Frontend.IntegrationTests/Frontend.IntegrationTests/Tests/Steps/PermissionSteps.cs
@@ -0,0 +1,34 @@
+﻿using CalculateFunding.Permissions.Clients.UsersClient.Models;
+using CalculateFunding.Permissions.Models;
+using Frontend.IntegrationTests.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Frontend.IntegrationTests.Tests.Steps
+{
+    [Binding]
+    public class PermissionSteps
+    {
+        [Given(@"the user '(.*)' has the following permissions for Funding Stream '(.*)'")]
+        public void GivenTheUserHasTheFollowingPermissionsForFundingStream(string userId, string fundingStreamId, Table table)
+        {
+            FundingStreamPermissionUpdateModel fundingStreamPermission = table.CreateInstance<FundingStreamPermissionUpdateModel>();
+            PermissionsHelper helper = new PermissionsHelper();
+
+            UserPermission userPermission = new UserPermission()
+            {
+                FundingStreamId = fundingStreamId,
+                Permissions = fundingStreamPermission,
+                UserId = userId,
+            };
+
+            helper.ApplyPermissions(userPermission).Wait();
+        }
+
+    }
+}
